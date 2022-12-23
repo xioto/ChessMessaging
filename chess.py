4349404b11abc00f281377d9bf332e0d2b8184c2
Euan Steven <euan@jollyjohn.me.uk>
2022-12-23 16:37:53 +0000

    Add files via upload

---
+++ chess.py
@@ -0,0 +1,12 @@
+string = str(input("What string do you want to translate?"))
+
+dict = {83: 'AB', 
+        84:'AB', 
+        85:'AB', 
+        86:'AB', 
+        87:'AB', 
+        88:'AB', 
+        89:'AB', 
+        90:'AB',}
+
+print(string.translate(dict))
